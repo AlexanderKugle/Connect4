6e03fb02950cf67fa98ddd27c5620d336ffcd484
Chry530 <98545951+Chry530@users.noreply.github.com>
2023-01-22 22:16:29 -0700

    little better

--- app/src/main/java/com/example/connect4/GameActivity.java
+++ app/src/main/java/com/example/connect4/GameActivity.java
@@ -33,6 +33,12 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_game);
